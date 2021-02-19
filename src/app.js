15ca62557fdaad5f54a0a63a61e4996026de114b
mgd2347 <margaridaf235@gmail.com>
2021-02-19 00:57:32 +0000

    Started adding real time data

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 function formatDate(date) {
-  console.log(date);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
   let weekDay = days[date.getDay()];
   let day = date.getDate();
@@ -17,5 +16,44 @@ function formatDate(date) {
   return (fullDate);
 }
 
+function displayWeather(response) {
+  console.log(response);
+  let city = response.data.name;
+  let country = response.data.sys.country;
+  document.querySelector("#city").innerHTML = `${city}, ${country}`;
+  document.querySelector("#description").innerHTML = response.data.weather[0].description;
+  document.querySelector("#icon").setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
+  document.querySelector("#temp").innerHTML = Math.round(response.data.main.temp);
+}
+
+function getCity(city) {
+  let units = "metric";
+  let apiKey = "6f57e84bdcf65c7e46537056925d0c97";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "6f57e84bdcf65c7e46537056925d0c97";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getLocation(event) {
+  navigator.geolocation.getCurrentPosition(getPosition);
+}
+
 let now = new Date();
 document.querySelector("#date").innerHTML = formatDate(now);
+
+getCity(`Lisbon`);
+
+document.querySelector("#current-location").addEventListener("click", getLocation);
