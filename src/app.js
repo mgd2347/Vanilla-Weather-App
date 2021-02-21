fc9591277b7b32aef4b7a92c830f32bd1463d076
mgd2347 <margaridaf235@gmail.com>
2021-02-21 22:09:15 +0000

    Added the forecast

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
-function formatDate(date) {
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-  let weekDay = days[date.getDay()];
-  let day = date.getDate();
-  let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  let month = months[date.getMonth()];
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -12,7 +8,19 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let fullDate = `${weekDay} ${day}, ${month} ${hours}:${minutes}`;
+  return `${hours}:${minutes}`;
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+  let weekDay = days[date.getDay()];
+  let day = date.getDate();
+  let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  let month = months[date.getMonth()];
+  
+  let fullDate = `${weekDay} ${day}, ${month} ${formatHours(timestamp)}`;
   return (fullDate);
 }
 
@@ -32,6 +40,38 @@ function displayWeather(response) {
   document.querySelector("#visibility").innerHTML = response.data.visibility / 1000;
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#hourly-forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+  
+    forecastElement.innerHTML += `
+      <div class="col-2">
+        <ul>
+          <li>
+            ${formatHours(forecast.dt * 1000)}
+          </li>
+          <li>
+            <img
+              src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+              alt="${forecast.weather[0].description}"
+            />
+          </li>
+          <li>
+            ${Math.round(forecast.main.temp)}°
+          </li>
+          <li>
+            ${forecast.weather[0].main}
+          </li>
+        </ul>
+      </div>
+    `;
+  } 
+}
+
 function getCity(city) {
   let units = "metric";
   let apiKey = "6f57e84bdcf65c7e46537056925d0c97";
@@ -39,6 +79,9 @@ function getCity(city) {
   let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
   
   axios.get(apiUrl).then(displayWeather);
+
+  let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(forecastUrl).then(displayForecast);
 }
 
 function searchCity(event) {
